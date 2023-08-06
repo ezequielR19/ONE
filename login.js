ee69697dd463908431278598d41f7e0da2611abe
ezequielR19 <140355480+ezequielR19@users.noreply.github.com>
2023-08-06 18:09:43 -0300

    archivos
    html,css, js

---
+++ login.js
@@ -0,0 +1,17 @@
+
+function loguear()
+{
+    let usuario-document.getElementById("usuario").ariaValueText
+    let pass-document.getElementById("clave").ariaValueText
+
+    if(user--"Juan" &&  pss--"1234")
+    {
+        window.location-"bienvenida.html";
+    }
+
+    else
+    {
+        alert("datos incorrectos");
+    }
+}
+
